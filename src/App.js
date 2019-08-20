2fa4286d62228042ecbd80776ffb6e556dfbdc7e
Vterebenin <niceserver11@mail.ru>
2019-08-20 23:49:12 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+window.onkeypress = handle;
+
+let lastTime = Date.now();
+let phrase = "";
+
+function handle(e) {
+  // let text = e.type +
+  //   ' key=' + e.key +
+  //   ' code=' + e.code +
+  //   (e.shiftKey ? ' shiftKey' : '') +
+  //   (e.ctrlKey ? ' ctrlKey' : '') +
+  //   (e.altKey ? ' altKey' : '') +
+  //   (e.metaKey ? ' metaKey' : '') +
+  //   (e.repeat ? ' (repeat)' : '') +
+  //   "\n";
+
+  let text = e.key;
+
+    
+  if (Date.now() - lastTime > 400) {
+    phrase = ""
+    // phrase += new Array(81).join('-') + '\n';
+  }
+  lastTime = Date.now();
+
+  phrase += text;
+  if (phrase === "ternow1308") {
+    var newDiv = document.createElement("div"); 
+    // and give it some content 
+    var newContent = document.createTextNode("Hi there and greetings!"); 
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent); 
+    document.body.appendChild(newDiv)
+    console.log("URA !!", phrase)
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      123qwe
     </div>
+
   );
 }
 
